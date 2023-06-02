f2ddb31e57034c0e45347ea55af0c97086107b2c
Eli Levit <levit@hey.com>
2023-06-02 17:27:34 +0300

    commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,32 +9,30 @@ export const meta: V2_MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex flex-col gap-24 p-4">
+    <div className="flex flex-col gap-24 overflow-hidden p-4">
       <h1 className="font-pilowlava text-7xl text-tree-poppy-500 md:leading-[1] lg:text-[120px]">
         I'm Bamba
       </h1>
-      <div className="relative mx-auto flex flex-col items-center justify-center gap-12 rounded-lg border-tree-poppy-700 bg-[length:400px] bg-repeat lg:flex-row lg:gap-24">
+      <div className="relative mx-auto flex flex-col items-center justify-center gap-4 rounded-lg border-tree-poppy-700 bg-[length:400px] bg-repeat md:gap-12 lg:flex-row lg:gap-24">
         <img
           src={Front}
           className="h-[250px] object-contain md:h-[500px] 2xl:h-[500px]"
         />
-        <p className="mx-auto mb-12 max-w-[300px] font-shantell text-3xl !leading-relaxed text-black md:max-w-[500px]">
-          <span className="text-tree-poppy-700">
-            I love to sleep, to play and to eat.{" "}
-          </span>
-          I'm 4 months old. I'm a mongrel mixed with Pit Bull. And I have{" "}
+        <p className="mx-auto font-shantell text-3xl !leading-relaxed text-black md:max-w-[300px] md:max-w-[500px] md:text-3xl">
+          I love to sleep, to play and to eat. I'm 4 months old. I'm a mongrel
+          mixed with Pit Bull. And I have
           <span className="font-bold text-tree-poppy-700">"white sock"</span> on
           my left hand ©
         </p>
       </div>
-      <div className="flex flex-row items-center justify-center gap-12">
+      <div className="flex flex-col items-center justify-center gap-12 md:flex-row">
         <p className="font-shantell text-3xl !leading-relaxed text-black md:max-w-[400px]">
           My father took me to the sea for the first time 😍 →
         </p>
-        <img src={Sea} className="w-[400px] rotate-6 drop-shadow-lg" />
+        <img src={Sea} className="w-[400px] rotate-6" />
       </div>
-      <div className="flex flex-row items-center justify-center gap-12">
-        <img src={Sleep} className="w-[400px] -rotate-12 drop-shadow-lg" />
+      <div className="flex flex-col items-center justify-center gap-12 md:flex-row">
+        <img src={Sleep} className="w-[400px] -rotate-12" />
         <p className="font-shantell text-3xl !leading-relaxed text-black md:max-w-[400px]">
           ← And that's how I'm sleeping
         </p>
